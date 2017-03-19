9295958d5b71f68dca0e845837999316c109899f
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-03-20 00:11:12 +1100

    few

--- JavaScript/ReactJS/project/SPA.js
+++ JavaScript/ReactJS/project/SPA.js
@@ -506,6 +506,41 @@ var Stuff = React.createClass({
 // http://stackoverflow.com/questions/40108843/react-how-to-load-and-render-external-html-file
 var componentTestInnerHtml = React.createClass({
 
+  componentDidMount: function() {
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('myImg');
+    var modalImg = document.getElementById("modal_img");
+    var captionText = document.getElementById("caption");
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+
+
+    // Click outside modal to close modal : 
+    modal.onclick = function(event) { 
+       if (event.target == modal) { // must use "event.target" otherwise modal closes on clicking the image
+           modal.style.display = "none";
+       }
+    }   
+  
+  },
+
   createMarkup: function() { 
 
     // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests
