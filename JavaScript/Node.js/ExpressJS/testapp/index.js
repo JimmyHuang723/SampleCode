be552848279d2632238b92e05104d1cbf07235fa
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-07-22 17:17:04 +1000

    wdwq

--- JavaScript/Node.js/ExpressJS/testapp/index.js
+++ JavaScript/Node.js/ExpressJS/testapp/index.js
@@ -1,16 +1,45 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/mongo', function (req, res) {
+
+app.post('/mongowrite', function (req, res) {
+  var MongoClient = require('mongodb').MongoClient
+
+  MongoClient.connect('mongodb://jimmy723:xxxjimmy723@care.hopto.org:27017/test', function(err, db) {
+  if (err) throw err;
+
+  //var myobj = { name: "Company Inc", address: "Highway 37" };
+  var myobj = req.body;
+
+  db.collection("test").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+
+    console.log("1 record inserted");
+    db.close();
+  });
+});
+
+  res.send('done!');
+});
+
+
+app.get('/mongoread', function (req, res) {
   var MongoClient = require('mongodb').MongoClient
 
   MongoClient.connect('mongodb://jimmy723:xxxjimmy723@care.hopto.org:27017/test', function (err, db) {
     if (err) throw err
-
+    
     db.collection('test').find().toArray(function (err, result) {
       if (err) throw err
       
@@ -18,13 +47,14 @@ app.get('/mongo', function (req, res) {
         console.log("-------------")
         console.log(member)
       })
-
-     
+      
+      res.json(result);
+      db.close(); 
     })
   })
-
-  res.send('mongo!')
 })
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
