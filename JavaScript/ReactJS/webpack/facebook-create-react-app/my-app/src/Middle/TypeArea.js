c2b5289534d5720b36e52bdc230546a47291121f
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-07-24 23:41:18 +1000

    dwqw

--- JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/TypeArea.js
+++ JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/TypeArea.js
@@ -13,14 +13,14 @@ class TypeArea extends React.Component {
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
     this.onTextChange = this.onTextChange.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
+    this.onInputFileChange = this.onInputFileChange.bind(this);
   }
   
   componentDidMount() {
 
   }
 
-  onInputChange(e) { 
+  onInputFileChange(e) { 
     var typeArea = this;
     console.log("fileInput.change ...");
     const file = e.target.files[0];
@@ -65,7 +65,7 @@ class TypeArea extends React.Component {
                   className="form-control" name="typearea" rows="5" placeholder="Type a message..."/>        
         <button type="button" className="btn btn-primary btn-block" onClick={this.handleClick}>Send</button>
         <input type="file" className=""
-               onChange={this.onInputChange}
+               onChange={this.onInputFileChange}
                name="file" accept=".jpg,.jpeg,.png,.gif,.bmp,.tiff"/>
         <img className="input-image-css" src={this.state.inputImage} />       
       </div>
