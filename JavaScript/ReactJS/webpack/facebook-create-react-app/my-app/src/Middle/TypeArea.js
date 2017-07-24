1593221c492183150c73fa19b0d85369e1f03256
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-07-24 21:47:28 +1000

    wdwq

--- JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/TypeArea.js
+++ JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/TypeArea.js
@@ -13,6 +13,34 @@ class TypeArea extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   
+  componentDidMount() {
+
+  }
+
+  onInputChange(e) { 
+    console.log("fileInput.change ...");
+    const file = e.target.files[0];
+    if (file) {
+      if (/^image\/\w+$/.test(file.type)) {
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function () {
+          //console.log("dataURL : " + reader.result);
+          var imageDataURL = reader.result;
+          document.getElementById('inputImage').setAttribute( 'src', imageDataURL );
+        };
+        reader.onerror = function (error) {
+          console.log('reader error: ', error);
+        };
+
+      } else {
+        window.alert('Please choose an image file.');
+      }
+    }else{
+      console.log(" files NULL ");  
+    }
+  }
+
   handleClick() {
     this.props.on_send(document.getElementById("middle_textarea").value);
     document.getElementById("middle_textarea").value = "";
@@ -23,6 +51,10 @@ class TypeArea extends React.Component {
       <div className="type-area">
         <textarea id="middle_textarea" className="form-control" name="typearea" rows="5" placeholder="Type a message..."/>        
         <button type="button" className="btn btn-primary btn-block" onClick={this.handleClick}>Send</button>
+        <input type="file" className=""
+               onChange={this.onInputChange}
+               name="file" accept=".jpg,.jpeg,.png,.gif,.bmp,.tiff"/>
+        <img className="input-image-css" id="inputImage" />       
       </div>
     );
   }
