00b62b27db52779350d185d3bd646276f4691fa9
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-08-16 12:57:03 +1000

    wdwd

--- JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/MessageChat.js
+++ JavaScript/ReactJS/webpack/facebook-create-react-app/my-app/src/Middle/MessageChat.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Lightbox from 'react-images'; // https://github.com/jossmac/react-images
+import Tooltip from 'rc-tooltip';
+import 'rc-tooltip/assets/bootstrap.css';
 
 class MessageChat extends React.Component {
   constructor(props) {
@@ -24,9 +26,24 @@ class MessageChat extends React.Component {
   render() {
     let image = null;
     if (this.props.message.img){
-      image =  <img className="chat-image-css" src={this.props.message.img} 
-                width={this.props.message.img_w} height={this.props.message.img_h} 
-                onClick={this.onClickImage}  />; 
+      image = (
+               <Tooltip
+                placement={'right'}
+                mouseEnterDelay={0}
+                mouseLeaveDelay={0.1}
+                destroyTooltipOnHide={false}
+                trigger={Object.keys( {hover: 1} ) }               
+                overlay={<div style={{ height: 50, width: 50 }}>Click to see full size</div>}
+                align={{
+                  offset: [4, 0],
+                }}
+                transitionName={"transitionName"}
+               >
+                 <img className="chat-image-css" src={this.props.message.img} 
+                  width={this.props.message.img_w} height={this.props.message.img_h} 
+                  onClick={this.onClickImage}  />
+               </Tooltip>
+               ); 
     }
 
     return (
