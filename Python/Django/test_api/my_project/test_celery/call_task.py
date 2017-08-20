e5ca6b3f6e7b5638d825b4fc799fec8f972050aa
friek723 <Jimmy@Jimmys-MacBook-Pro.local>
2017-08-20 21:11:19 +1000

    wd

---
+++ Python/Django/test_api/my_project/test_celery/call_task.py
@@ -0,0 +1,17 @@
+from tasks import add
+
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+result = add.delay(4, 4)
+
+#print result.get(timeout=10)
+#print result.ready()
+
+
+
---
+++ Python/Django/test_api/my_project/test_celery/tasks.py
@@ -0,0 +1,10 @@
+from celery import Celery
+import time
+
+app = Celery('tasks', backend='rpc://', broker='amqp://jimmy723:jimmy723@localhost:5672')
+
+@app.task
+def add(x, y):
+    time.sleep(5)    
+    return x + y
+
